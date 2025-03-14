9190eacbddea95c16eed987a96b37240430de19f
s1n4u <seka4ort@gmail.com>
2025-03-14 16:17:22 +0000

    + manifest

---
+++ hr_hospital/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    'name': 'Hospital Management',
+    'summary': '',
+    'author': 's1n',
+    'website': 'https://odoo.school/',
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '17.0.2.2.0',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': [],
+    },
+
+    'data': [
+
+        'security/ir.model.access.csv',
+
+        'views/hr_hospital_menu.xml',
+        'views/hr_hospital_doctor_views.xml',
+        'views/hr_hospital_patient_views.xml',
+        'views/hr_hospital_disease_views.xml',
+        'views/hr_hospital_visit_views.xml',
+
+    ],
+    'demo': [
+
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+    'images': [
+        'static/description/icon.png'
+    ],
+
+
+}
+
