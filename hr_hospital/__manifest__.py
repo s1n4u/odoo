94e308f5799db9848dd69321db12dfde467d8623
s1n4u <seka4ort@gmail.com>
2025-03-21 14:24:32 +0000

    4

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -1,40 +1,30 @@
 {
-    'name': 'Hospital Management',
-    'summary': '',
+
+    'name': 'Hospital Management Extended',
+    'summary': 'Extended hospital module with diagnoses, hierarchy and wizards',
     'author': 's1n',
     'website': 'https://odoo.school/',
-    'category': 'Customizations',
+    'category': 'Healthcare',
     'license': 'OPL-1',
     'version': '17.0.2.2.0',
-
-    'depends': [
-        'base',
-    ],
-
-    'external_dependencies': {
-        'python': [],
-    },
-
+    'depends': ['base'],
     'data': [
-
         'security/ir.model.access.csv',
 
-        'views/hr_hospital_menu.xml',
-        'views/hr_hospital_doctor_views.xml',
-        'views/hr_hospital_patient_views.xml',
+        'data/disease_demo.xml',
+        'data/demo_specialties.xml',
+        'data/demo_persons.xml',
 
+        'views/person_views.xml',
+        'views/doctor_views.xml',
+        'views/patient_views.xml',
+        'views/disease_views.xml',
+        'views/visit_views.xml',
+        'views/diagnosis_views.xml',
+        'views/menu.xml',
+        'views/specialty_views.xml',
+        'views/set_personal_doctor_wizard_view.xml',
     ],
-    'demo': [
-
-    ],
-
     'installable': True,
-    'auto_install': False,
-
-    'images': [
-        'static/description/icon.png'
-    ],
-
-
+    'application': True
 }
-
--- hr_hospital/models/__init__.py
+++ hr_hospital/models/__init__.py
@@ -1,4 +1,1 @@
-from . import hr_hospital_doctor
-from . import hr_hospital_patient
-from . import hr_hospital_disease
-from . import hr_hospital_visit
+from . import person, doctor, patient, disease, visit, diagnosis, specialty
